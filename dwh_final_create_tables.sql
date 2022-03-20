d5b4c18a7d609cbe9a12cca305877b11a03aa1ae
AlexeyOm <alexey.omelchenko@gmail.com>
2022-03-20 12:59:28 +0300

    начал работу над календарём

--- dwh_final_create_tables.sql
+++ dwh_final_create_tables.sql
@@ -6,10 +6,10 @@ create table bookings.fact_flights (
 	arrival_fact timestamp,
 	departure_delay int,
 	arrival_delay int,
-	airplane_id int,
+	aircraft_id int,
 	departure_airport_id int,
 	arrival_airport_id int,
-	fare_conditions varchar(20)	
+	fare_conditions int	
 );
 
 comment on column bookings.fact_flights.pax_id is 'Идентификатор пассажира';
@@ -17,43 +17,65 @@ comment on column bookings.fact_flights.departure_fact is 'Дата и время вылета (
 comment on column bookings.fact_flights.arrival_fact is 'Дата и время прилета (факт)';
 comment on column bookings.fact_flights.departure_delay is 'Задержка вылета (разница между фактической и запланированной датой в секундах)';
 comment on column bookings.fact_flights.arrival_delay is 'Задержка прилета (разница между фактической и запланированной датой в секундах)';
-comment on column bookings.fact_flights.airplane_id is 'Идентификатор самолёта';
+comment on column bookings.fact_flights.aircraft_id is 'Идентификатор самолёта';
 comment on column bookings.fact_flights.departure_airport_id is 'Идентификатор аэропорта вылета';
 comment on column bookings.fact_flights.arrival_airport_id is 'Идентификатор аэропорта прилёта';
 comment on column bookings.fact_flights.fare_conditions is 'Класс обслуживания';
 
 
-select distinct jsonb_object_keys(contact_data) from bookings.tickets; --убедимся, что в контактных данных есть только email и phone
-select max(length(passenger_name)) from bookings.tickets t ; --проверим длину имён, чтобы взять с запасом
-
 drop table if exists bookings.dim_pax;
 
 create table bookings.dim_pax (
-	id int,
 	pax_id int,
 	name varchar(50),
 	phone varchar(10),
 	email varchar(50)
 );
 
-comment on column bookings.dim_pax.id is 'суррогатный ключ';
 comment on column bookings.dim_pax.pax_id is 'идентификатор пассажира';
 comment on column bookings.dim_pax.name is 'фамилия и имя пассажира латиницей';
 comment on column bookings.dim_pax.phone is 'телефон';
 comment on column bookings.dim_pax.email is 'электронная почта';
 
 
+drop table if exists bookings.dim_planes;
 
-
-drop table if exists bookings.dim_pax;
-
-create table bookings.dim_pax (
-	id int,
-	pax_id int,
-	name varchar(50),
-	phone varchar(10),
-	email varchar(50)
+create table bookings.dim_aircrafts (
+	id varchar(3),
+	model varchar(50),
+	range int
 );
 
+comment on column bookings.dim_aircrafts.id is 'код самолёта IATA';
+comment on column bookings.dim_aircrafts.model is 'модель самолёта';
+comment on column bookings.dim_aircrafts.range is 'максимальное расстояние';
 
 
+drop table if exists bookings.dim_tariff;
+
+create table bookings.dim_tariff (
+	id int,
+	tarif varchar(10)
+);
+
+comment on column bookings.dim_tariff.id is 'id тарифа';
+comment on column bookings.dim_tariff.tarif is 'название тарифа';
+
+
+drop table if exists bookings.dim_airports;
+
+create table bookings.dim_airports (
+	id varchar(3),
+	name varchar(50),
+	city varchar(50),
+	longtitude float(8),
+	latitude float(8),
+	timezone varchar(20)
+);
+
+comment on column bookings.dim_airports.id is 'id';
+comment on column bookings.dim_airports.name is 'название аэропорта';
+comment on column bookings.dim_airports.city is 'город, в котором или рядом с которым расположен аэропорт';
+comment on column bookings.dim_airports.longtitude is 'координаты: долгота аэропорта';
+comment on column bookings.dim_airports.latitude is 'координаты: широта аэропорта';
+comment on column bookings.dim_airports.timezone is 'часовой пояс';
